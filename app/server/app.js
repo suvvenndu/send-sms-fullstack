f2d631b3be7bea349dc3c6f49c9101cefbf34c06
Subhendu Swain <suvendu.swain@gmail.com>
2021-09-09 18:40:13 +1000

    Update to server side Docker to run express and update to express API for running over docker/aws

--- app/server/app.js
+++ app/server/app.js
@@ -1,64 +1,70 @@
-const express = require('express');
-const bodyparser = require('body-parser');
+const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
-require('dotenv').config();
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
+require("dotenv").config();
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const session = require("express-session");
 
-
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
 
 var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: process.env.AUTH0_JWKS_URI
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.AUTH0_JWKS_URI,
   }),
   audience: process.env.AUTH0_AUDIENCE,
   issuer: process.env.AUTH0_ISSUER,
-  algorithms: ['RS256']
+  algorithms: ["RS256"],
 });
 
+app.use(
+  session({
+    secret: "1355d63f-152e-49c8-8a88-cd244359c250",
+    cookie: {
+      httpOnly: true,
+      //secure: true,
+      maxAge: 60 * 60 * 1000,
+    },
+  })
+);
 app.use(jwtCheck);
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 
-if (app.get('env') === 'production') {
-  // Use secure cookies in production (requires SSL/TLS)
-  sess.cookie.secure = true;
-
+if (app.get("env") === "production") {
   // Uncomment the line below if your application is behind a proxy (like on Heroku)
   // or if you're encountering the error message:
   // "Unable to verify authorization request state"
   // app.set('trust proxy', 1);
 }
 
+app.post("/text", (req, res) => {
+  console.log("Req", req.body);
 
-app.post('/text', (req, res) => {
+  var params = {
+    Message: req.body.message,
+    PhoneNumber: "+" + req.body.number,
+    MessageAttributes: {
+      "AWS.SNS.SMS.SenderID": {
+        DataType: "String",
+        StringValue: req.body.subject,
+      },
+    },
+  };
 
-    console.log('Req', req.body);
+  var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
+    .publish(params)
+    .promise();
 
-    var params = {
-        Message: req.body.message,
-        PhoneNumber: '+' + req.body.number,
-        MessageAttributes: {
-            'AWS.SNS.SMS.SenderID': {
-                'DataType': 'String',
-                'StringValue': req.body.subject
-            }
-        }
-    };
-
-    var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
-
-    publishTextPromise.then(
-        function (data) {
-            res.end(JSON.stringify({ MessageID: data.MessageId }));
-        }).catch(
-            function (err) {
-                res.end(JSON.stringify({ Error: err }));
-            });
-
+  publishTextPromise
+    .then(function (data) {
+      res.end(JSON.stringify({ MessageID: data.MessageId }));
+    })
+    .catch(function (err) {
+      res.end(JSON.stringify({ Error: err }));
+    });
 });
 
-app.listen(5000, () => console.log('SMS Service Listening on PORT 5000'))
+app.listen(5000, () => console.log("SMS Service Listening on PORT 5000"));
